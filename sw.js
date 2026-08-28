11b9f301ad849fdde23afd5bdb5a9c2e33cc5e51
JoRuXo <JoRuXo@hotmail.com>
2026-08-28 23:37:07 +0200

    Snapshot local antes de sincronizar con el repo remoto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'mis-gastos-v16';
+var CACHE = 'mis-gastos-v17';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
